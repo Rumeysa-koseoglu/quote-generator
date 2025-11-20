1b2c8fdff6ece0b1d69a3c245187cb43bfe6700a
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-20 15:50:10 +0300

    feat: create base structure of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import QuoteCard from "./components/QuoteCard";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [quote, setQuote] = useState();
+  const [author, setAuthor] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const handleGenerate = () => {
+    console.log("button is clicked");
+  };
+
+  return (
+    <>
+      <QuoteCard quote={quote} author={author} />
+      <Button onClick={handleGenerate} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function QuoteCard({ quote, author }) {
+  return (
+    <div>
+      {/**quote content
+       *
+       */}
+    </div>
+  );
+}
+
+export default QuoteCard;
