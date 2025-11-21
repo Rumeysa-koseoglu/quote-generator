52873302b3db16239489caaac19793d220a5b3a2
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-21 19:40:20 +0300

    feat: implement quote generation functionality and display on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import "./App.css";
 import QuoteCard from "./components/QuoteCard";
 import Button from "./components/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [quote, setQuote] = useState();
-  const [author, setAuthor] = useState();
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleGenerate = () => {
-    console.log("button is clicked");
+  const handleGenerate = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch("https://dummyjson.com/quotes");
+
+      const result = await response.json();
+
+      const allQuotes = result.quotes;
+      const randomIndex = Math.floor(Math.random() * allQuotes.length);
+      const randomQuote = allQuotes[randomIndex];
+
+      setQuote(randomQuote.quote);
+      setAuthor(randomQuote.author);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
