38b2646f79c3a3b23cc726176f15a42b4130c981
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-23 13:15:03 +0300

    style(button): apply Tailwind styles and improve responsive layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function Button({ onClick }) {
   return (
-    <div>
-      <button onClick={onClick}>Generate Quote</button>
+    <div className="w-full flex justify-center">
+      <button
+        className="bg-[#616950] text-[#f3e8d4] rounded-[10px] text-[20px] font-lucida w-full md:max-w-[750px] sm:max-w-[550px]
+  h-[45px]
+  p-2 mx-6"
+        onClick={onClick}
+      >
+        Generate Quote
+      </button>
     </div>
   );
 }
