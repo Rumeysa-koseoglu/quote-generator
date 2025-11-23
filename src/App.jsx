23aa6779672d49c1e3007c55acf73e174c59e3fa
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-23 13:08:45 +0300

    style(app): apply background image and layout styling to main container

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,10 @@ function App() {
   };
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col items-center justify-center gap-6 bg-[url('/DABDA7B2-CF7F-42E7-8005-788C2A98A059.png')] bg-cover bg-no-repeat">
       <QuoteCard quote={quote} author={author} />
       <Button onClick={handleGenerate} />
-    </>
+    </div>
   );
 }
 
