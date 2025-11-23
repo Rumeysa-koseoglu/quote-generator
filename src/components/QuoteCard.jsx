b95cad18546b3d1d6e1ce2a5d0072b5e1682577e
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-23 13:18:49 +0300

    style(card): add responsive layout and styling to quote card

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function QuoteCard({ quote, author }) {
   return (
-    <div>
-      <div className="card">
+    <div className="w-full flex justify-center">
+      <div className="flex flex-col items-center justify-center gap-8 text-[#e6dad0] text-18px bg-[#424c388a] rounded-[40px] mx-6 p-8 shadow-xl hover:shadow-2xl font-lucida w-full md:max-w-[750px] sm:max-w-[550px] h-[340px] ">
         {quote ? (
           <>
             <p>'{quote}'</p>
-            <span>{author}</span>
+            <span className="mr-2">{author}</span>
           </>
         ) : (
           <span>click generate to get a quote..</span>
