1fbcde889a0f7079f2371fcde5e7c7b7b281d9eb
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-24 16:04:04 +0300

    style: improve button styling

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ onClick }) {
   return (
     <div className="w-full flex justify-center">
       <button
-        className="bg-[#616950] text-[#f3e8d4] rounded-[10px] text-[20px] font-lucida w-full md:max-w-[750px] sm:max-w-[550px]
+        className="bg-[#eae2c0] text-[#616950] active:scale-100 duration-100 active:bg-[#616950] active:text-[#eae2c0] rounded-[10px] text-[20px] font-lucida w-full md:max-w-[750px] sm:max-w-[550px]
   h-[45px]
   p-2 mx-6"
         onClick={onClick}
