52aa4123439fd6bdcc52db90a6aaabc229e69939
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-24 16:04:29 +0300

    style: imrove quote card styling

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -3,14 +3,16 @@ import React from "react";
 function QuoteCard({ quote, author }) {
   return (
     <div className="w-full flex justify-center">
-      <div className="flex flex-col items-center justify-center gap-8 text-[#e6dad0] text-18px bg-[#424c388a] rounded-[40px] mx-6 p-8 shadow-xl hover:shadow-2xl font-lucida w-full md:max-w-[750px] sm:max-w-[550px] h-[340px] ">
+      <div className="flex flex-col items-center justify-center gap-10 text-[#e6dad0] bg-[#424c388a] rounded-[40px] mx-6 p-8 shadow-xl hover:shadow-2xl w-full md:max-w-[750px] sm:max-w-[550px] h-[340px] ">
         {quote ? (
           <>
-            <p>'{quote}'</p>
-            <span className="mr-2">{author}</span>
+            <p className="font-lucida text-[20px] my-8">'{quote}'</p>
+            <span className="mr-2 text-[18px] font italic ">-{author}</span>
           </>
         ) : (
-          <span>click generate to get a quote..</span>
+          <span className="text-[#e6dad0] text-[20px] italic font-verdana">
+            click generate to get a quote..
+          </span>
         )}
       </div>
     </div>
