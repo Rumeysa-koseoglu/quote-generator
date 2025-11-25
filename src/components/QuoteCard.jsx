cfb3cdf409c64a61c471e4301c9f79ecf94e830c
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-11-25 21:50:02 +0300

    feat: improve QuoteCard with spinner and fade-in effect during loading

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import loadingSpinner from "../assets/spinner.svg";
+import Spinner from "./Spinner";
 
-function QuoteCard({ quote, author }) {
+function QuoteCard({ quote, author, loading }) {
   return (
     <div className="w-full flex justify-center">
-      <div className="flex flex-col items-center justify-center gap-10 text-[#e6dad0] bg-[#424c388a] rounded-[40px] mx-6 p-8 shadow-xl hover:shadow-2xl w-full md:max-w-[750px] sm:max-w-[550px] h-[380px] ">
-        {quote ? (
-          <>
-            <p className="font-lucida text-[20px] my-8">'{quote}'</p>
-            <span className="mr-2 text-[18px] font italic ">-{author}</span>
-          </>
+      <div className="flex flex-col items-center justify-center gap-10 text-[#e6dad0] bg-[#424c388a] rounded-[40px] mx-6 p-8 shadow-xl hover:shadow-2xl w-full md:max-w-[750px] sm:max-w-[550px] h-[380px]">
+        {loading ? (
+          <Spinner className="w-12 h-12 text-[#253526] animate-spin" />
         ) : (
-          <span className="text-[#e6dad0] text-[20px] italic font-verdana">
-            click generate to get a quote..
-          </span>
+          <>
+            {quote ? (
+              <>
+                <p
+                  className={`font-lucida text-[20px] my-8 ${
+                    loading ? "fade-in" : ""
+                  }`}
+                >
+                  '{quote}'
+                </p>
+                <span className="mr-2 text-[18px] font italic ">-{author}</span>
+              </>
+            ) : (
+              <span className="text-[#e6dad0] text-[20px] italic font-verdana">
+                click 'Generate Quote' to get a quote..
+              </span>
+            )}
+          </>
         )}
       </div>
     </div>
